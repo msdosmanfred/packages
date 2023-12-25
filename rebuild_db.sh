28a89f7dc1787cb0432aa5a18bf60a4554d49d0c
MS-DOS Manfred <hoefer9@gmail.com>
2023-12-25 18:10:43 +0100

    Updating db

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -6,17 +6,17 @@ echo "###########################"
 cd x86_64
 rm -f custom*
 
-repo-add -n -R -s custom.db.tar.gz *.pkg.tar.zst
+repo-add -n -R custom.db.tar.gz *.pkg.tar.zst
 
 rm custom.db
-rm custom.db.sig
+#rm custom.db.sig
 rm custom.files
-rm custom.files.sig
+#rm custom.files.sig
 
 mv custom.db.tar.gz custom.db
-mv custom.db.tar.gz.sig custom.db.sig
+#mv custom.db.tar.gz.sig custom.db.sig
 mv custom.files.tar.gz custom.files
-mv custom.files.tar.gz.sig custom.files.sig
+#mv custom.files.tar.gz.sig custom.files.sig
 
 echo "#######################################"
 echo "Packages in the x86_64 Repo are up2date!"
