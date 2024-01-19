b9d54971cf0c3a95cb5d5f47cd1d0c66f3234c56
MS-DOS Manfred <hoefer9@gmail.com>
2024-01-19 20:01:51 +0100

    Updating db

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -7,19 +7,19 @@ echo -e "\t# Building the x86_64 Repo DB. #"
 echo -e "\t################################${normal}"
 
 cd x86_64
-rm -f arschlive*
+rm -f arschlive-repo*
 
-repo-add -n -R arschlive.db.tar.gz *.pkg.tar.zst &> /dev/null
+repo-add -n -R arschlive-repo.db.tar.gz *.pkg.tar.zst &> /dev/null
 
-rm arschlive.db
-#rm arschlive.db.sig
-rm arschlive.files
-#rm arschlive.files.sig
+rm arschlive-repo.db
+#rm arschlive-repo.db.sig
+rm arschlive-repo.files
+#rm arschlive-repo.files.sig
 
-mv arschlive.db.tar.gz arschlive.db
-#mv arschlive.db.tar.gz.sig arschlive.db.sig
-mv arschlive.files.tar.gz arschlive.files
-#mv arschlive.files.tar.gz.sig arschlive.files.sig
+mv arschlive-repo.db.tar.gz arschlive-repo.db
+#mv arschlive-repo.db.tar.gz.sig arschlive-repo.db.sig
+mv arschlive-repo.files.tar.gz arschlive-repo.files
+#mv arschlive-repo.files.tar.gz.sig arschlive-repo.files.sig
 
 git add .
 git commit -am "Updating db"
